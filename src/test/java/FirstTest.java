69b2841d7bcff598e894bd4060a987904ce98fe3
Ana Khurtsidze <anakhurtsidze@credo.ge>
2024-11-22 19:41:04 +0400

    practice commit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,2 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class FirstTest {
+
+
+    @Test
+    public  void Test(){
+        System.setProperty("Webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver  = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        driver.quit();
+    }
 }
